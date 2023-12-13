9707cc496fb56d56dd004bc529eef11fd89eabd3
gunmentoy <tomnguyen0443@gmail.com>
2023-12-12 20:39:53 -0800

    added a button component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <h1>Hello World</h1>
+  <div class="container">
+    <Header title="Task Tracker" />
+  </div>
 </template>
 
 <script>
+import Header from './components/Header';
+
 export default {
   name: 'App',
-  components: {},
+  components: {
+    Header,
+  },
 };
 </script>
 
