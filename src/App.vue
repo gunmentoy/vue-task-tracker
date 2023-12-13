2bb6aa93ffa985671e3025978d6cdcca595f41a7
gunmentoy <tomnguyen0443@gmail.com>
2023-12-12 21:45:56 -0800

    added a way to remove the tasks

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,7 @@ export default {
   },
   methods: {
     deleteTask(id) {
+      if(confirm('Are you sure you want to delete this task?'))
       this.tasks = this.tasks.filter((task) => task.id !== id);
     }
   },
